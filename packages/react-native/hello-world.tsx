3004fec1398c395af2161f73a97c6be5ebcb2cf7
KATT <alexander@n1s.se>
2021-09-10 15:57:19 +0200

    make flatlist

--- packages/react-native/hello-world.tsx
+++ packages/react-native/hello-world.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import { trpc } from '@zart/react/trpc';
+import { FlatList } from 'react-native';
 
 export function HelloWorld() {
   const posts = trpc.useQuery(['post.all'], {
     refetchInterval: 3000,
   });
   return (
-    <View style={styles.container}>
-      <Text>{posts.status}</Text>
-      {posts.data?.map((post) => (
-        <Text key={post.id}>{post.title}</Text>
-      ))}
-    </View>
+    <SafeAreaView style={styles.container}>
+      {posts.data ? (
+        <FlatList
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => {
+                alert(`You clicked ID ${item.id} `);
+              }}
+            >
+              <View style={styles.item}>
+                <Text>{item.title}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          data={posts.data}
+          keyExtractor={(item) => item.id}
+        />
+      ) : (
+        <Text>{posts.status}</Text>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  item: {
+    flex: 1,
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
   },
 });
